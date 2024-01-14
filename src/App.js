4d0c38fa4791feab4fbf61699d8bee8ecc209843
Jay-JayG <jjgochez@gmail.com>
2024-01-13 21:42:26 -0800

    pre-auto deploy changes

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          Hello from gochezj
+        This app was deployed by GitHub automatically for gochezj
         </p>
       </div>
     );
